8d378345ad44f54e9e0a68b0fff181e95c15478a
Balogh Etele <baloghetele285@gmail.com>
2021-08-03 21:51:05 +0200

    Added User model

---
+++ frontend/src/app/shared/models/user.model.ts
@@ -0,0 +1,22 @@
+export class User {
+  username: string;
+
+  constructor(username: string) {
+    this.username = username;
+  }
+
+  login(){
+  }
+
+  logout(){
+  }
+
+  sendmessage(){
+  }
+
+  changeSettings(){}
+
+  cry(){
+    console.log("I just cryin' for no good reason, pls send help");
+  }
+}
