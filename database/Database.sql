bf1d124b44eb8fef032da3287ddd4cdc20a1ceec
Yndiliadrin <baloghetele285@gmail.com>
2021-08-05 21:34:36 +0200

    Databasegenerator script patch

--- database/Database.sql
+++ database/Database.sql
@@ -1,9 +1,15 @@
-DROP DATABASE IF EXISTS ccdb;
-DROP TABLE IF EXISTS ccdb.User;
 DROP TABLE IF EXISTS ccdb.Message;
+DROP TABLE IF EXISTS ccdb.User;
 DROP TABLE IF EXISTS ccdb.Room;
--- CREATE USER ccadmin IDENTIFIED BY 'password';
+DROP DATABASE IF EXISTS ccdb;
+
+
+-- Create admin user for the applications database
+DROP USER IF EXISTS 'ccadmin'@'localhost';
+CREATE USER 'ccadmin'@'localhost' IDENTIFIED BY 'password';
 CREATE DATABASE ccdb;
+-- The privileges can use some fine tuneing
+
 CREATE TABLE ccdb.User (
     id          VARCHAR(36)     NOT NULL,
     username    VARCHAR(32),
@@ -27,3 +33,8 @@ CREATE Table ccdb.Room (
     id          VARCHAR(36)     NOT NULL,
     PRIMARY KEY (id)
 );
+
+
+-- Cannot add privileges to by my user
+-- GRANT ALL PRIVILEGES ON ccdb.* TO 'ccadmin'@'localhost';
+--FLUSH PRIVILEGES;
