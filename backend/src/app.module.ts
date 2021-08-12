f6083258afd37b6aae4b3db160e000a1b691cac1
Yndiliadrin <baloghetele285@gmail.com>
2021-08-12 13:57:17 +0200

    #4 added user and message MVC to backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { MessageModule } from './message/message.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, MessageModule],
   controllers: [AppController],
   providers: [AppService],
 })
